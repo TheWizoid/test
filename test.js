349fdad1c62065f4a69f7b1ae125e3a9e8db288d
Charlie Moore <cm9083@gmail.com>
2018-01-09 16:17:33 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+"use strict";
+function printfunction(){
+  document.writeln(arg_amount(arguments));
+  switch(arguments.length){
+    case 0:
+      document.writeln("nice!");
+      break;
+    default:
+      for(var i=0;i<arguments.length;i++){
+        document.writeln(arguments[i] + " ")
+      }
+  }
+}
+function arg_amount(){
+  return arguments[0].length;
+}
+printfunction("a","b","c","adfadsf")
+alert("eyup!!!!!!!!")
+//printfunction() //doesnt do anything
